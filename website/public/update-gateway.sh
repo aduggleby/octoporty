b08313e3b2b09cbc64a746f715d1c8d3c1223753
Alexander Duggleby <151508+aduggleby@users.noreply.github.com>
2026-01-28 11:27:43 +0100

    docs: update documentation

--- website/public/update-gateway.sh
+++ website/public/update-gateway.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # update-gateway.sh
 # Octoporty Gateway update script
-# Version: 0.9.0
+# Version: 0.9.6
 # Usage: curl -fsSL https://octoporty.com/update-gateway.sh | bash
 
 set -e
 
 INSTALL_DIR="/opt/octoporty/gateway"
-SCRIPT_VERSION="0.9.0"
+SCRIPT_VERSION="0.9.6"
 
 cat << 'EOF'
 
